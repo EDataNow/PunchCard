04bfea4ad0c8a9019ade57492e6a32c9f4538588
Joshua <joshuavancamp@gmail.com>
2016-02-17 16:41:56 -0500

    Fixing dependent syntax

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,4 @@
 class Assignment < ActiveRecord::Base
-  belongs_to :users, dependent :destroy
-  belongs_to :shifts, dependent :destroy
+  belongs_to :users, dependent: :destroy
+  belongs_to :shifts, dependent: :destroy
 end
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,4 @@
 class Shift < ActiveRecord::Base
-  belongs_to :locations, dependent :destroy
+  belongs_to :locations, dependent: :destroy
   has_many :users, through: :assignments
 end
