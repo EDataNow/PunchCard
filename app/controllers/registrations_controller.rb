4ff5651a8eefe1bd4783274ebbbb00d6695a7221
Spencer Hanley <shanley2@me.com>
2016-02-22 15:13:01 -0500

    Minor visual changes being made - Logout button, greeting, devise form sizing & positioning

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  before_filter :configure_permitted_parameters, :only => [:create]
+  before_filter :sign_up_params, :only => [:create]
 
   private
 
   def sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
   end
 
 end
