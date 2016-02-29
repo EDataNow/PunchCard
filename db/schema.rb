0a3b309576984c407c8eb67d3278ee4a96cebe62
Spencer Hanley <shanley2@me.com>
2016-02-29 12:18:28 -0500

    Adding Admin Boolean
    Boolean value added to user model for defining if a user is an
    administrator or not.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223153059) do
+ActiveRecord::Schema.define(version: 20160229171611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160223153059) do
     t.integer  "failed_attempts",        default: 0,  null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
