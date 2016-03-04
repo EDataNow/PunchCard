e772a1cd8bda15081f5f7ad193fd40152c10c916
Joshua <joshuavancamp@gmail.com>
2016-03-04 12:43:59 -0500

    Fixing json acceptance

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,17 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
 
-  def create
-    render :json => {
-        'csrfParam' => request_forgery_protection_token,
-        'csrfToken' => form_authenticity_token
-    }
-  end
 
+  def create
+    respond_to do |format|
+      format.html { super }
+      format.json {
+        warden.authenticate!(scope: resource_name, recall: "#{controller_path}#new")
+        render status: 200, json: {
+          error: "Success",
+          'csrfParam': request_forgery_protection_token,
+          'token': form_authenticity_token }
+      }
+    end
+  end
 end
