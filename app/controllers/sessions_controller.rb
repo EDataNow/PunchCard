9da0ff06b053c25065604483a66b96c114637dbe
Joshua <joshuavancamp@gmail.com>
2016-03-10 10:48:04 -0500

    Modifying Login JSON

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,18 @@ class SessionsController < Devise::SessionsController
     respond_to do |format|
       format.html { super }
       format.json {
-        @locations = {}
-        @assignment = Assignment.new
-        current_user.locations.each do |loc|
-        #Location.all.each do |loc|
-          @locations[loc.id] = {shift: loc.shifts.active.to_json(include: {assignments: {include: {user: {only: [:first_name, :last_name, :email]} }}}), name: loc.name}
-        end
-
         warden.authenticate!(scope: resource_name, recall: "#{controller_path}#new")
+        @assignment = Assignment.new
+        @active_assignment = true if (Assignment.find_by(user_id: current_user.id) != nil)
+        @locations = {}
+        current_user.locations.each do |loc|
+          @locations[loc.id] = {active_shift: loc.shifts.active.as_json(include: {assignments: {include: {user: {only: [:first_name, :last_name, :email]} }}}), name: loc.name}
+        end
         render json: {
             notice: "Success",
-            user: current_user.as_json,
-            locations: @locations.as_json,
-            active_assignment: Assignment.find_by(user_id: current_user.id).as_json,
-            current_shift: Shift.find_by(id: assign_to_shift(1)).as_json,
+            user: current_user,
+            locations: @locations,
+            active_assignment: @active_assignment,
           },
           status: 200
       }
--- app/models/location.rb
+++ app/models/location.rb
@@ -13,7 +13,10 @@
 
 class Location < ActiveRecord::Base
   has_many :shifts
+
+  has_many :workplaces
   has_many :users, through: :workplaces, foreign_key: :user_id#, dependent: :destroy
+
   validates :name, presence: true
 
   def self.search(search)
